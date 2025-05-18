5155a8813ff7dda8b4027197f2714c03ffa0aeab
abakedjoetato <thry360@hotmail.com>
2025-05-18 19:33:36 +0000

    Clean

--- temp_unzip/EKF_V0.9-main/finder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Running mvn directly to ensure the bot runs"
-mvn exec:java -Dexec.mainClass="com.deadside.bot.Main"
--- temp_unzip/EKF_V0.9-main/update_premium_links.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Update all premium URLs in the ButtonListener.java file
-sed -i 's|https://deadside.com/premium|https://emeraldskillfeed.tip4serv.com/|g' src/main/java/com/deadside/bot/listeners/ButtonListener.java
-sed -i 's|https://deadside.com/premium|https://emeraldskillfeed.tip4serv.com/|g' com/deadside/bot/listeners/ButtonListener.java
-sed -i 's|https://deadside.com/premium|https://emeraldskillfeed.tip4serv.com/|g' com/deadside/bot/premium/FeatureGate.java
-
-echo "Updated premium links to point to Tip4serv website"
